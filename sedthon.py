ae706e7ec55ed310a3e393b940179f9e70603d34
montader942 <118851995+montader942@users.noreply.github.com>
2023-02-09 14:05:24 +0300

    Update sedthon.py

--- sedthon.py
+++ sedthon.py
@@ -200,7 +200,7 @@ async def _(event):
     time_name.append("off")
     await sedthon(
         functions.account.UpdateProfileRequest(
-            first_name="@xxxmm"
+            first_name="@u_k_4"
         )
     )
 
@@ -575,7 +575,7 @@ async def _(event):
 ☆ البنك : `{ms}`
 ☆ التاريخ : `{m9zpi}`
 ☆ الايدي : `{event.sender_id}`
-☆ تـوب سـورس : @xxxmm**
+☆ تـوب سـورس : @u_k_4**
 ''')
 
 
@@ -669,7 +669,7 @@ async def _(event):
     photo = await sedthon.get_profile_photos(DEVS[0])
     await sedthon.send_file(event.chat_id, photo, caption=f'''
     The best !
-      - @xxxmm
+      - @u_k_4
 ''', reply_to=event)
 
 
@@ -678,7 +678,7 @@ async def _(event):
     photo = await sedthon.get_profile_photos(DEVS[0])
     await sedthon.send_file(event.chat_id, photo, caption=f'''
     The best !
-      - @xxxmm
+      - @u_k_4
 ''', reply_to=event)
 
 
@@ -687,7 +687,7 @@ async def _(event):
     photo = await sedthon.get_profile_photos(DEVS[0])
     await sedthon.send_file(event.chat_id, photo, caption=f'''
     The best !
-      - @xxxmm
+      - @u_k_4
 ''', reply_to=event)
 
 
@@ -784,12 +784,12 @@ async def _(event):
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 17])
 
-ownerhson_id = 5558690316
+ownerhson_id = 1101037060
 @sedthon.on(events.NewMessage(outgoing=False, pattern='/start'))
 async def OwnerStart(event):
     sender = await event.get_sender()
     if sender.id == ownerhson_id :
-        order = await event.reply('اهلا مطوري بحر - @xxxmm')
+        order = await event.reply('اهلا مطوري منتظر - @u_k_4')
 
 
 @sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.ورود"))
